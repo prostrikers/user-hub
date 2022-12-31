3c81aedbd0ca35a5b6ad05052a4978a307112342
z9fr <z9fr@protonmail.com>
2022-12-31 14:41:44 +0530

    fix: added coming soon for header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,21 @@
 import PropTypes from "prop-types";
 import { styled } from "@mui/material/styles";
-import { Box, Stack, AppBar, Toolbar, IconButton, Button } from "@mui/material";
+import {
+  Box,
+  Stack,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Button,
+  Typography,
+} from "@mui/material";
 import AccountPopover from "./AccountPopover";
 import Iconify from "../Iconify";
 import { bgBlur } from "../../utils/styles";
 import { useUserStore } from "../../store/createUserSlice";
 import { Link } from "react-router-dom";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { useEffect, useState } from "react";
 
 const HEADER_MOBILE = 64;
 
@@ -20,18 +29,47 @@ const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   },
 }));
 
+const NavItems = [
+  {
+    name: "Membership",
+    link: "/coming-soon",
+  },
+  {
+    name: "Shop & rentals ",
+    link: "/coming-soon",
+  },
+  {
+    name: "Training",
+    link: "/coming-soon",
+  },
+
+  {
+    name: "Events",
+    link: "/coming-soon",
+  },
+];
+
 Header.propTypes = {
   onOpenNav: PropTypes.func,
 };
 
-export default function Header({
-  onOpenNav,
-  removeBlur,
-}: {
-  onOpenNav: any;
-  removeBlur?: boolean;
-}) {
+export default function Header({ onOpenNav }: { onOpenNav: any }) {
   const { user } = useUserStore();
+
+  const [width, setWidth] = useState<number>(window.innerWidth);
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  const isMobile = width <= 768;
 
   //@ts-ignore
   const StyledRoot = styled(AppBar)(({ theme }: { theme: DefaultTheme }) => ({
@@ -57,9 +95,33 @@ export default function Header({
           <Iconify icon="eva:menu-2-fill" />
         </IconButton>
 
-        <Box component={Link} to="/">
-          <img src="/logo.png" />
-        </Box>
+        <Stack
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={5}
+        >
+          <Box component={Link} to="/">
+            <img src="/logo.png" />
+          </Box>
+
+          {isMobile ? null : (
+            <>
+              {NavItems.map((item) => {
+                return (
+                  <Typography
+                    style={{ color: "#333333", textDecoration: "none" }}
+                    variant="h6"
+                    component={Link}
+                    to={item.link}
+                  >
+                    {item.name}
+                  </Typography>
+                );
+              })}
+            </>
+          )}
+        </Stack>
 
         <Box sx={{ flexGrow: 1 }} />
 
@@ -79,22 +141,26 @@ export default function Header({
           >
             {user ? (
               <>
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  size="large"
-                  style={{
-                    borderRadius: 20,
-                    textTransform: "none",
-                    paddingLeft: 35,
-                    paddingRight: 35,
-                    background: "#06283D",
-                  }}
-                  component={Link}
-                  to="/"
-                >
-                  Book now
-                </Button>
+                {isMobile ? null : (
+                  <>
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      size="large"
+                      style={{
+                        borderRadius: 20,
+                        textTransform: "none",
+                        paddingLeft: 35,
+                        paddingRight: 35,
+                        background: "#06283D",
+                      }}
+                      component={Link}
+                      to="/"
+                    >
+                      Book now
+                    </Button>
+                  </>
+                )}
 
                 <AccountPopover />
               </>
